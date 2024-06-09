5c9ae3e7f40f55434dc91f6d061fcd79b7f206fc
AkhilGandhi1999 <akhil.gandhi10.ag@gmail.com>
2024-06-09 17:33:21 -0600

    Loaded the rule 4 image

--- src/lib/data/assets.ts
+++ src/lib/data/assets.ts
@@ -8,6 +8,7 @@ const a = (light: string, dark?: string): Asset =>
 	dark ? { dark: gh(dark), light: gh(light) } : gh(light);
 
 const Assets = {
+	Rule4: a('rule4.jpeg'),
 	Profile: a('profile.jpeg'),
 	AWS: a('aws.svg'),
 	Bootstrap: a('bootstrap.svg'),
