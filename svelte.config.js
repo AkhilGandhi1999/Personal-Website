21615a56d2f3d1cd4b34771375eb94998155d061
AkhilGandhi1999 <akhil.gandhi10.ag@gmail.com>
2024-06-09 19:50:06 -0600

    Updated the ci cd script

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,19 @@ const config = {
 		adapter: adapter({ fallback: '404.html' }),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? base : ''
-		}
+		},
+		// prerender: {
+		// 	handleHttpError: ({ path, referrer, response }) => {
+		// 	  if (response.status === 500) {
+		// 		// this will still be available in the page store
+		// 		response.body = {
+		// 		  ...response.body,
+		// 		  title: 'Error',
+		// 		  message: 'An unexpected error occurred'
+		// 		};
+		// 	  }
+		// 	}
+		//   }
 	}
 };
 
